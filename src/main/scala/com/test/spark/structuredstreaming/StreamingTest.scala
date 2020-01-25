e00b1bfa5bd44a8df7f6641ceb8b559f97aa25f7
Azim Kangda <azim.u.kangda@gmail.com>
2020-01-25 16:31:05 +0530

    Sample Streaming Code

--- src/main/scala/com/test/spark/structuredstreaming/StreamingTest.scala
+++ src/main/scala/com/test/spark/structuredstreaming/StreamingTest.scala
@@ -1,14 +1,32 @@
 package com.test.spark.structuredstreaming
 import context._
 
-import org.apache.spark.sql.SparkSession
-
 object StreamingTest {
 
   def main(args: Array[String]): Unit = {
 
-    
+    import spark.implicits._
 
+    val df = spark.readStream
+      .format("kafka")
+      .option(
+        "kafka.bootstrap.servers",
+        "wn01.itversity.com:6667,wn02.itversity.com:6667,wn03.itversity.com:6667,wn04.itversity.com:6667")
+      .option("subscribe", "test_structuredstreming")
+      .load()
+      .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+      .as[(String, String)]
+
+    val query = df.writeStream
+      .format("parquet")
+      .option("checkpointLocation", "/user/rajeshkr/checkpoint")
+      .option("path", "/user/rajeshkr/data")
+      .option("format", "append")
+      .option("startingOffsets", "earliest")
+      .outputMode("append")
+      .start()
+
+    query.awaitTermination()
   }
 
 }
