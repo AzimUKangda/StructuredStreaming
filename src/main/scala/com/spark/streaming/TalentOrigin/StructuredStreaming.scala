b6745a402a32deea0bb7d30c8d8458637450aa55
RAJESH <bigdatacloudsolution@gmail.com>
2020-01-29 06:47:18 +0530

    Schema Correction

--- src/main/scala/com/spark/streaming/TalentOrigin/StructuredStreaming.scala
+++ src/main/scala/com/spark/streaming/TalentOrigin/StructuredStreaming.scala
@@ -7,37 +7,37 @@ import org.apache.log4j._
 
 object StructuredStreaming {
   def main(args: Array[String]): Unit = {
-    val spark  = SparkSession.builder().appName("Spark Structured Streaming").master("local[*]").getOrCreate()
+    val spark = SparkSession.builder().appName("Spark Structured Streaming").master("local[*]").getOrCreate()
     //spark.sparkContext.setLogLevel("ERROR")
     Logger.getLogger("org").setLevel(Level.ERROR)
 
-    val retailDataSchema = new StructType()
-      .add("SerialNumber", IntegerType)
-      .add("InvoiceNo", IntegerType)
-      .add("StockCode", StringType)
-      .add("Description", StringType)
-      .add("Quantity", IntegerType)
-      .add("InvoiceDate", StringType)
-      .add("UnitPrice", DoubleType)
-      .add("CustomerId", LongType)
-      .add("Country", StringType)
-      .add("InvoiceTimestamp", StringType)
+    val retailDataSchema = StructType(Array(StructField("SerialNumber", IntegerType, true),
+      StructField("InvoiceNo", StringType, true),
+      StructField("StockCode", StringType, true),
+      StructField("Description", StringType, true),
+      StructField("Quantity", IntegerType, true),
+      StructField("InvoiceDate", TimestampType, true),
+      StructField("UnitPrice", DoubleType, true),
+      StructField("CustomerID", DoubleType, true),
+      StructField("Country", StringType, true),
+      StructField("InvoiceTimestamp", TimestampType, true)))
+
 
     val streamingData = spark
+      //.read.schema(retailDataSchema).option("header", "true").csv("C:/Users/RAJESH/Desktop/Structured-Streaming/TalentOrigin/temp_working") //.drop("SerialNumber")
       .readStream
-      .schema(retailDataSchema)
-      .csv("C:/Users/RAJESH/Desktop/Structured-Streaming/TalentOrigin/temp_working").drop("SerialNumber")
+      .schema(retailDataSchema).option("header", "true")
+      .csv("C:/Users/RAJESH/Desktop/Structured-Streaming/TalentOrigin/source-code-master/datasets/retail-data") //.drop("SerialNumber")
 
+   val filteredData = streamingData//.filter("Country = 'United Kingdom'")
 
-    val filteredData = streamingData//.filter("Country = 'United Kingdom'")
+     val query = filteredData.writeStream
+       .format("console")
+       .queryName("filteredByCountry")
+       .option("truncate","false")
+       .outputMode(OutputMode.Update())
+       .start()
 
-    val query = filteredData.writeStream
-      .format("console")
-      .queryName("filteredByCountry")
-      .option("truncate","false")
-      .outputMode(OutputMode.Append())
-      .start()
-
-    query.awaitTermination()
+     query.awaitTermination()
   }
 }
